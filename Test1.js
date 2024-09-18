a778b62903e68e21137e113371f2219fe314758a
Peter VosBurgh <pvosbur@protonmail.com>
2024-09-18 07:32:07 -0400

    Added Test1

---
+++ Test1.js
@@ -0,0 +1,15 @@
+/*
+ * Created by User: petervosburgh
+ * Date: 9/18/24
+ * Time: 7:28 AM
+ * 
+ */
+function Test1()
+{
+  configObject();
+
+  function configObject()
+  {
+    console.log( "In Config")
+  }
+}
